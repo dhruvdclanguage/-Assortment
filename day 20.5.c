e5a27f473a1625d2e0e3faf98ce04c1fe622c925
dhruvdclanguage <126230133+dhruvdclanguage@users.noreply.github.com>
2023-03-16 12:42:43 +0530

    Add files via upload

---
+++ day 20.5.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+ 
+ int main () {
+	
+	int i , j , n , m , sum=0;
+	
+	printf ("Enter Size of Row :");
+	scanf ("%d" ,&m);
+	
+	printf ("Enter Size of Col :");
+	scanf ("%d" ,&n);
+	
+	int arr [m] [n];
+	
+	for (i=0;i<m;i++){
+		
+		for (j=0;i<n;j++){
+			 
+			 printf ("Enter Elements [%d][%d] :",i,j);
+		}
+	}
+	   for (i=0;i<m;i++){
+	   	
+	   	for (j=0;j<n;j++){
+	   		
+	   		printf ("%d",arr [i][j]);
+		   }
+		   
+		   printf ("\n");
+	   }
+	   
+	     for (i=0;i<n;i++){
+	     	
+	     	sum=0;
+	     	
+	     for (j=0;j<m;j++){
+	     	
+	     	sum+=arr[j][i];
+		 }	
+		  printf ("Sum of col Wise :%d",sum);
+		  
+		  printf ("\n");
+		 }
+		 
+		 return 0;
+	}
